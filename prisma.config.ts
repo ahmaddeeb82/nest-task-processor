50c6753ab890b5a848abb298def4b98e6f35131e
Deeb <deebahmad2023@gmail.com>
2026-01-21 02:38:19 +0300

    wip

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,12 +1,18 @@
 import "dotenv/config";
-import { defineConfig, env } from "prisma/config";
+import { defineConfig } from "prisma/config";
+
+const DATABASE_URL =
+  process.env.DATABASE_URL ??
+  "postgresql://user:pass@localhost:5432/db?schema=public";
 
 export default defineConfig({
   schema: "prisma/schema.prisma",
+
   migrations: {
     path: "prisma/migrations",
   },
+
   datasource: {
-    url: env("DATABASE_URL"),
+    url: DATABASE_URL,
   },
 });
